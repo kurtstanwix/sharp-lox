c70eb578469cdedb8c81ad1d5bd1ad188e3e06c1
Kurt Stanwix <kurtstanwix@gmail.com>
2023-09-05 22:50:06 +1000

    Begin scanning

--- Program.cs
+++ Program.cs
@@ -1,3 +1,69 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.IO;
 
-Console.WriteLine("Hello, World!");
+namespace SharpLox;
+
+class Program
+{
+    private static bool HadError = false;
+    
+    static int Main(string[] args)
+    {
+        if (args.Length > 1)
+        {
+            Console.WriteLine(("UsageL sharplox [script]"));
+            return 64;
+        }
+        if (args.Length == 1)
+        {
+            return RunFile(args[0]);
+        }
+        return RunPrompt();
+    }
+
+    private static int RunFile(string path)
+    {
+        var fileContents = File.ReadAllText(Path.GetFullPath(path));
+        Run(fileContents);
+
+        if (HadError) return 65;
+        
+        return 0;
+    }
+
+    private static int RunPrompt()
+    {
+        while (true)
+        {
+            Console.Write("> ");
+            var line = Console.ReadLine();
+            if (line is null) break;
+            Run(line);
+            HadError = false;
+        }
+
+        return 0;
+    }
+
+    private static void Run(string source)
+    {
+        var scanner = new Scanner(source);
+        var tokens = scanner.ScanTokens();
+
+        foreach (var token in tokens)
+        {
+            Console.WriteLine(token);
+        }
+    }
+
+    public static void Error(int line, string message)
+    {
+        Report(line, "", message);
+    }
+
+    private static void Report(int line, string where, string message)
+    {
+        Console.Error.WriteLine($"[line {line}] Error{where}: {message}");
+        HadError = true;
+    }
+}
---
+++ Scanner.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using SharpLox.Tokens;
+
+namespace SharpLox;
+
+public class Scanner
+{
+    private readonly string _source;
+    private readonly List<Token> _tokens = new();
+    private int _start, _current, _line = 1;
+
+    public Scanner(string source)
+    {
+        _source = source;
+    }
+
+    public IEnumerable<Token> ScanTokens()
+    {
+        while (!IsAtEnd())
+        {
+            _start = _current;
+            ScanToken();
+        }
+        
+        _tokens.Add(new Token(TokenType.Eof, "", null, _line));
+        return _tokens.ToList();
+    }
+    
+    private void ScanToken() {
+        var c = Advance();
+        switch (c) {
+            case '(': AddToken(TokenType.LeftParen); break;
+            case ')': AddToken(TokenType.RightParen); break;
+            case '{': AddToken(TokenType.LeftBrace); break;
+            case '}': AddToken(TokenType.RightBrace); break;
+            case ',': AddToken(TokenType.Comma); break;
+            case '.': AddToken(TokenType.Dot); break;
+            case '-': AddToken(TokenType.Minus); break;
+            case '+': AddToken(TokenType.Plus); break;
+            case ';': AddToken(TokenType.Semicolon); break;
+            case '*': AddToken(TokenType.Star); break;
+            case '!':
+                AddToken(Match('=') ? TokenType.BangEqual : TokenType.Bang);
+                break;
+            case '=':
+                AddToken(Match('=') ? TokenType.EqualEqual : TokenType.Equal);
+                break;
+            case '<':
+                AddToken(Match('=') ? TokenType.LessEqual : TokenType.Less);
+                break;
+            case '>':
+                AddToken(Match('=') ? TokenType.GreaterEqual : TokenType.Greater);
+                break;
+            case '/':
+                if (Match('/'))
+                {
+                    while (Peek() != '\n' && !IsAtEnd()) Advance();
+                }
+                else
+                {
+                    AddToken(TokenType.Slash);
+                }
+                break;
+            default:
+                Program.Error(_line, "Unexpected character.");
+                break;
+        }
+    }
+
+    private char Peek()
+    {
+        if (IsAtEnd()) return '\0';
+        return _source[_current];
+    }
+
+    private char Advance()
+    {
+        return _source[_current++];
+    }
+    
+    private bool Match(char expected) {
+        if (IsAtEnd()) return false;
+        if (_source[_current] != expected) return false;
+
+        _current++;
+        return true;
+    }
+
+    private void AddToken(TokenType type)
+    {
+        AddToken(type, null);
+    }
+
+    private void AddToken(TokenType type, object literal)
+    {
+        var text = _source.Substring(_start, _current - _start);
+        _tokens.Add(new Token(type, text, literal, _line));
+    }
+
+    private bool IsAtEnd()
+    {
+        return _current >= _source.Length;
+    }
+}
